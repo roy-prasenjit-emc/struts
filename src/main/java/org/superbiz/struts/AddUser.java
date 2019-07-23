423572bcae8d0b3e519a1e693d89974ce3a513a0
Prasenjit Roy <prasenjit.roy@emc.com>
2019-07-23 10:26:12 +0530

    struts to spring

--- src/main/java/org/superbiz/struts/AddUser.java
+++ src/main/java/org/superbiz/struts/AddUser.java
@@ -17,11 +17,24 @@
 */
 package org.superbiz.struts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import java.util.Properties;
 
+@Component
 public class AddUser {
+
+    private final UserService service;
+
+    @Autowired
+    public AddUser(UserService service) {
+        this.service = service;
+    }
+
 
     private int id;
     private String firstName;
@@ -60,15 +73,11 @@ public class AddUser {
         this.id = id;
     }
 
+
+    @Transactional
     public String execute() {
 
         try {
-            UserService service = null;
-            Properties props = new Properties();
-            props.put(Context.INITIAL_CONTEXT_FACTORY,
-                "org.apache.openejb.core.LocalInitialContextFactory");
-            Context ctx = new InitialContext(props);
-            service = (UserService) ctx.lookup("UserServiceImplLocal");
             service.add(new User(id, firstName, lastName));
         } catch (Exception e) {
             this.errorMessage = e.getMessage();
